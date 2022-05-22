a69af2532c090ce11d48e0d68cb5123f9725cc63
MaruzaMartins <maruzacampelo@gmail.com>
2022-05-22 16:46:26 -0300

    first commit

--- src/one/dio/basecamp/Main.java
+++ src/one/dio/basecamp/Main.java
@@ -27,6 +27,13 @@ public class Main {
         Emprestimo.calcular(1000,Emprestimo.getTresParcelas());
         Emprestimo.calcular(1000,5);
 
+
+        //Área dos quadriláteros
+        System.out.println("Exercício Quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d, 5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
     }
 
 
